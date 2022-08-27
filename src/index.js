58fd81f742a8c9497341f913704ae007c8c94136
Viktor Rudrnko <viktorrudenko93@gmail.com>
2022-08-27 15:46:44 +0300

    added the forecast at the bottom

--- src/index.js
+++ src/index.js
@@ -33,7 +33,36 @@ function todayIS() {
 todayIS();
 
 // final project
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
 
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `   
+      <div class="col-2">
+      <div class="weather-forecast-date">
+       ${day}
+      </div> 
+      <img src="https://openweathermap.org/img/wn/04d@2x.png" 
+       alt=""
+        width="50"/> 
+        <div class="weather-forecast-temperatures">
+         <span class="weather-forecast-temperature-max">
+          17°
+          </span>
+         <span class="weather-forecast-temperature-min">
+        15°
+         </span>
+        </div>
+        </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 //
 
 function showTemperature(response) {
@@ -80,6 +109,7 @@ function searchCity(city) {
 
 let searchButton = document.querySelector("#cityForm");
 searchButton.addEventListener("submit", search);
+displayForecast();
 
 //
 
